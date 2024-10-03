0ba99c47f71dc1a69ac8cc4fc6ef67612602a433
Alessandro Gonzaga <alexmichaelgonzaga@gmail.com>
2024-10-03 17:46:14 -0400

    updated resume and experience

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// App.js
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
@@ -12,35 +12,34 @@ import ChatGnzagaProject from './pages/ChatGnzagaProject';
 import NotFound from './pages/404';
 import ErrorPage from './pages/Error';
 import Footer from './components/Footer';
-import Experience from './pages/experience';
+import Experience from './pages/Experience';
 import PlaylistGeneratorProject from './pages/PlaylistProject';
 import PortfolioProject from './pages/PortfolioProject';
+import ChessGame from './pages/ChessGame';
 
 function App() {
   return (
     <Router>
-        <div className="bg-slate-900 min-h-screen flex flex-col">
-
+      <div className="bg-slate-900 min-h-screen flex flex-col">
         <Navbar />
         <div className="flex-grow">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/projects/task-management" element={<TaskManagementProject />} />
-          <Route path="/projects/PlaylistProject" element={<PlaylistGeneratorProject />} />
-          <Route path="/projects/discord-bot" element={<DiscordBotProject />} />
-          <Route path="/projects/chat-gnzaga" element={<ChatGnzagaProject />} />
-          <Route path="/projects/portfolio-project" element={<PortfolioProject />} />
-          <Route path="/experience" element={<Experience />} />
-          <Route path="/error" element={<ErrorPage />} />
-          <Route path="*" element={<NotFound />} />
-
-
-        </Routes>
-      </div>
-      <Footer />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/chess" element={<ChessGame />} />
+            <Route path="/projects/task-management" element={<TaskManagementProject />} />
+            <Route path="/projects/PlaylistProject" element={<PlaylistGeneratorProject />} />
+            <Route path="/projects/discord-bot" element={<DiscordBotProject />} />
+            <Route path="/projects/chat-gnzaga" element={<ChatGnzagaProject />} />
+            <Route path="/projects/portfolio-project" element={<PortfolioProject />} />
+            <Route path="/experience" element={<Experience />} />
+            <Route path="/error" element={<ErrorPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
     </Router>
   );
