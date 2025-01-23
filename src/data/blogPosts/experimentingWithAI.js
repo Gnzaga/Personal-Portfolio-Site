c7bb26780eb41407d5fa27201b7cc388cd09cb0e
Alessandro Gonzaga <alexmichaelgonzaga@gmail.com>
2025-01-23 17:40:32 -0500

    updayte blog posts and add images

--- src/data/blogPosts/experimentingWithAI.js
+++ src/data/blogPosts/experimentingWithAI.js
@@ -10,7 +10,9 @@ export default {
       "After testing the Ollama CLI for a while, I ended up installing OpenWebUI via Docker. This is a UI for interacting with Ollama, and it's incredibly useful. I can see the models I have running and interact with the models via the UI. It has features also like multiple user accounts, integration with CAS/LDAP, and the ability to make custom models within the UI. You're able to create custom models with custom system instructions, along with upload files to vectorized stores to use with RAG.",
       "I also realized that there is an application to be had implementing this to my portfolio website. By using RAG with some custom system prompting, I should be able to make a custom model which can serve as an assistant to help people learn more about me. I plan on implementing this soon.",
       "Another application I realize is that I can connect this to my discord bot using the Ollama API, so that my discord bot can be much, much more interactive. I plan on implementing this soon as well.",
-      "The next thing that I also should be able to do, is use this to teach others about how to use AI/ML particularly LLM's like my dad, sister and family & friends."
+      "The next thing that I also should be able to do, is use this to teach others about how to use AI/ML particularly LLM's like my dad, sister and family & friends. I've let quite a few of them know and have given them accounts",
+      "",
+      "Update: as of January, 2025 I have 18 users on chat.gnzaga.com. They are all friends and family, and I have been able to teach them how to use the system. I have also been able to implement a chatbot on my portfolio website that has access to some information regarding my resume and experience. At the bottom of the website, there is a perpetual chat button that will pull up a window for you to talk to the bot.",
     ],
     "tags": [
         { label: "AI", url: "/projects?filter=AI" },
@@ -22,7 +24,8 @@ export default {
     "links": [
         { label: "Related Projects", url: "/projects?filter=AI" },
         { label: "Open WebUI", url: "https://openwebui.com/" },
-        { label: "Ollama", url: "https://ollama.com/" }
+        { label: "Ollama", url: "https://ollama.com/" },
+        { label: "chat.gnzaga.com", url: "https://chat.gnzaga.com/" }
     ]
   };
   
