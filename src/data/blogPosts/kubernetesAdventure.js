9efce3b28509dc4ffde43c5eb0e44db2331d1aca
Alessandro Gonzaga <alessandromg02@gmail.com>
2025-01-23 16:52:23 -0500

    update to server.js, blogPosts, Blog.js, BlogDetail.js

--- src/data/blogPosts/kubernetesAdventure.js
+++ src/data/blogPosts/kubernetesAdventure.js
@@ -6,6 +6,19 @@ export default {
     "paragraphs": [
       "Today, I officially started my Kubernetes journey! After months of working with Docker, I’ve been itching to take things to the next level. To kick things off, I bought three HP Prodesk G3s and spent the afternoon upgrading them with RAM and storage. Each machine is now running Debian and ready to join the cluster.",
       "I’ve set up K3s and am experimenting with MetalLB as a load balancer. My goal is to deploy my portfolio website to this cluster and learn everything I can about container orchestration. It’s a big step forward, and I’m excited to see where this journey leads."
+      /*
+        I have wanted to work with kubernetes for a good amount of time now; howver, I haven't seemed to get it working how I wanted it to go yet. I tried running it on virtual machines, and I was able to get it working, but it didn't feel right running projects in containers on Kubernetes inside VM's on my ProxMox server. In any case, I still had some other projects to work on and so I wasn't pressed to really learn it. But it always remained a topic of interest to me. 
+
+        Eventually, in late December I stumbled across 3 HP Mini PC's with intel i5 7500T processors. They didn't come with any RAM or Hard drives, but at $100 plus shipping for the entire lot, I couldn't pass it up. I bought them and they arrived in early January. I spent a few days cleaning them up and installing RAM and storage into them. I kitted each with 16gb of ram, and a 250gb nvme drive. I spent an evening last week installing Debian on each, working out the hostnames and assigning IP addresses to each, along with setting up SSH, ansible and other tools I would need to manage them.
+
+        After I was able to get each of these set up with tools - as well as mapping my network storage Samba drive to each of them, I was ready to start setting up the cluster.
+
+        I decided to go with K3s for this cluster 
+
+
+
+
+      */
     ],
     "tags": [
         { label: "AI", url: "/projects?filter=AI" },
